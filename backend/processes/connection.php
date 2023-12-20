98c3bcd456c56cadedf0cb6da5a7384dad87e4af
dariil <santiagojohndaryll@gmail.com>
2023-12-20 18:15:02 +0800

    p

--- backend/processes/connection.php
+++ backend/processes/connection.php
@@ -2,7 +2,7 @@
     $server = "localhost";
     $root = "root";
     $password = "";
-    $db = "db_aa";
+    $db = "db_aa-merge";
 
     $conn = mysqli_connect($server, $root, $password, $db) or die("Error: Unable to Connect to Database.");
 ?>
